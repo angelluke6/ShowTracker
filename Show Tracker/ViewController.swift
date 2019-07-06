40b7a2875a1b11e822d3eaad5781d7da35414f0a
angelluke6 <azarrilli2004@gmail.com>
2019-07-05 22:36:02 -0400

    Initial Commit

---
+++ Show Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Show Tracker
+//
+//  Created by Jason Kornfield on 7/5/19.
+//  Copyright © 2019 AJ inc. Designs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
