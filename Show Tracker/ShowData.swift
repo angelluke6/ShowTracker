fae9e455705d067da2719874804c830580389973
angelluke6 <azarrilli2004@gmail.com>
2019-07-05 22:52:04 -0400

    added some files

---
+++ Show Tracker/ShowData.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  Show Tracker
+//
+//  Created by Jason Kornfield on 7/5/19.
+//  Copyright © 2019 AJ inc. Designs. All rights reserved.
+//
+
+import Foundation
+
+class ShowData {
+    let showTitle : String
+    let season : String
+    let episode : String
+    let done : Bool
+    init(showTitle: String, season: String, episode: String, done: Bool) {
+        self.showTitle = showTitle
+        self.season = season
+        self.episode = episode
+        self.done = done
+        
+    }
+}
